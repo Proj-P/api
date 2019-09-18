261aaf21481a6b5053bcfd13223700484067575d
Teun Kelting <teun@tjuna.com>
2019-09-18 13:20:54 +0200

    Moved error handlers to main (required)

--- projectp/__init__.py
+++ projectp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -24,6 +24,29 @@ def create_app(debug=False):
     from projectp.locations import locations
     from projectp.visits import visits
     from .main import main
+
+    # The 404 response
+    @app.errorhandler(404)
+    def page_not_found(error):
+        success = False
+        response = {
+            'success': success,
+            'error': {
+                'type': error.__class__.__name__,
+                'message': 'page not found'
+            }
+        }
+        return jsonify(response), 404
+
+    # The 405
+    @app.errorhandler(405)
+    def error405(e):
+        return "405 Wrong method", 405
+
+    # And the internal server error
+    @app.errorhandler(500)
+    def error500(e):
+        return "500 Internal server error", 500
 
     # Register blueprints
     app.register_blueprint(main)

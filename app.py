a01e20fbc34243fe9d8dc0326089c38ae43bdfc7
Teun Kelting <teun@tjuna.com>
2019-09-19 10:05:14 +0200

    Change port to 8080

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from projectp import create_app, socketio
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
